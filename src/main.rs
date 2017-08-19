1e6fedb1bcaa69a3b07801db939c644f5d741f1f
tsub <tsubasatakayama511@gmail.com>
2017-08-19 21:23:07 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,22 @@ use slack::RtmClient;
 use std::{env, process};
 
 fn main() {
-    let api_key = match env::var("SLACK_API_TOKEN") {
-        Ok(val) => val,
-        Err(_) => {
-            println!("Required the SLACK_API_TOKEN environment variable");
-            process::exit(1);
-        }
-    };
+    let api_key: String = api_key();
     let mut handler = handler::Handler;
     let r = RtmClient::login_and_run(&api_key, &mut handler);
 
     match r {
         Ok(_) => {},
         Err(err) => panic!("Error: {}", err)
+    }
+}
+
+fn api_key() -> String {
+    match env::var("SLACK_API_TOKEN") {
+        Ok(val) => val,
+        Err(_) => {
+            println!("Required the SLACK_API_TOKEN environment variable");
+            process::exit(1);
+        }
     }
 }
